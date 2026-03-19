902f46985beaf6a7db3d029803622954823ba2e1
Beedit <68902418+Beedit@users.noreply.github.com>
2026-03-19 00:15:02 +0000

    IArea was broken before. its not now

--- src/mongoDB/interfaces/IArea.ts
+++ src/mongoDB/interfaces/IArea.ts
@@ -1,14 +1,14 @@
 import { Types } from "mongoose";
 
 export interface IArea {
-    name: {
-        type: string,
-        owner: Types.ObjectId,
-        world: Types.ObjectId[],
+    name: string,
+    owner: Types.ObjectId,
+    world: Types.ObjectId,
         
-        description?: string,
-        areas?: Types.ObjectId[],
-        npcs?: Types.ObjectId[],
-        rootArea?: Types.ObjectId[]
-    }
+    description?: string,
+    dmOnlyNotes?: string,
+    areas?: Types.ObjectId[],
+    npcs?: Types.ObjectId[],
+    rootArea?: Types.ObjectId[],
+
 }
